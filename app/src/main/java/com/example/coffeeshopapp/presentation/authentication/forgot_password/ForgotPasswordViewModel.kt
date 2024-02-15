ab8366cde1293f529b0de90e9cb2a2990e8cfbdd
Cristina Onica <onicacristinaelena@yahoo.com>
2024-02-15 19:11:54 +0200

    forgot password screen

---
+++ app/src/main/java/com/example/coffeeshopapp/presentation/authentication/forgot_password/ForgotPasswordViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.coffeeshopapp.presentation.authentication.forgot_password
+
+import androidx.lifecycle.ViewModel
+
+class ForgotPasswordViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
