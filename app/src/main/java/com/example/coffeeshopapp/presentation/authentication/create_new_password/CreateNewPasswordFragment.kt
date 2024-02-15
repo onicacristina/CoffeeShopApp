455d1d1c9516b2d09ccdf9730bf51374bc8e149c
Cristina Onica <onicacristinaelena@yahoo.com>
2024-02-15 19:36:16 +0200

    create new password screen

---
+++ app/src/main/java/com/example/coffeeshopapp/presentation/authentication/create_new_password/CreateNewPasswordFragment.kt
@@ -0,0 +1,40 @@
+package com.example.coffeeshopapp.presentation.authentication.create_new_password
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.example.coffeeshopapp.R
+import com.example.coffeeshopapp.databinding.FragmentCreateNewPasswordBinding
+import com.example.coffeeshopapp.databinding.FragmentForgotPasswordBinding
+import com.example.coffeeshopapp.presentation.authentication.forgot_password.ForgotPasswordViewModel
+import com.example.coffeeshopapp.presentation.base.NoBottomNavigationFragment
+import com.example.coffeeshopapp.presentation.utils.extensions.viewBinding
+
+class CreateNewPasswordFragment :
+    NoBottomNavigationFragment<FragmentCreateNewPasswordBinding>(R.layout.fragment_create_new_password) {
+
+    override val viewBinding: FragmentCreateNewPasswordBinding by viewBinding(
+        FragmentCreateNewPasswordBinding::bind
+    )
+    override val viewModel: CreateNewPasswordViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+        initListeners()
+    }
+
+    private fun initViews() {
+        viewBinding.toolbar.tvTitle.text = getString(R.string.create_new_password)
+    }
+
+    private fun initListeners() {
+        viewBinding.toolbar.ivBack.setOnClickListener {
+            navController.popBackStack()
+        }
+    }
+}
